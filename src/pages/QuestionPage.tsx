df03a1036d6d17b88bf6fec606dd9cb4e7d0d495
kachmazoff <alek.kachmazov@yandex.ru>
2021-02-03 00:00:51 +0400

    Create QuestionModule

--- src/pages/QuestionPage.tsx
+++ src/pages/QuestionPage.tsx
@@ -3,9 +3,15 @@ import { useSelector } from "react-redux";
 import { Layout, Typography } from "antd";
 import { getIsAuthenticated } from "@/modules/AuthModule";
 import { BasePage } from "./components";
+import { QuestionModule } from "@/modules/QuestionModule/QuestionModule";
 
 export const QuestionPage = ({ match }) => {
   const questionId = match.params.id;
   const isAuthenticated = useSelector(getIsAuthenticated);
-  return <BasePage>{`Question ${questionId}`}</BasePage>;
+  return (
+    <BasePage>
+      {`Question ${questionId}`}
+      <QuestionModule questionId={questionId} />
+    </BasePage>
+  );
 };
