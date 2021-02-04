f2df67560b9ac7876999c2f7518fce1daccdeaf1
kachmazoff <alek.kachmazov@yandex.ru>
2021-02-04 16:56:56 +0400

    Create extended question interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,4 +1,4 @@
 export { ICard } from "./ICard";
 export { ICollectionInfo } from "./ICollectionInfo";
-export { IQuestion } from "./IQuestion";
+export { IQuestion, IQuestionExtended } from "./IQuestion";
 export { IAnswer } from "./IAnswer";
