2e4326bf60bff35f2a5a2d81fa9eeaa2aed9a957
kachmazoff <alek.kachmazov@yandex.ru>
2021-02-19 17:48:09 +0400

    Update render of CollectionsListModule

--- src/modules/CollectionsListModule/CollectionsListModule.tsx
+++ src/modules/CollectionsListModule/CollectionsListModule.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Empty, Typography } from "antd";
+import { Empty } from "antd";
 import { ICollectionInfo } from "@/interfaces";
 import { RootState } from "@/store";
 import { getAllCollections } from "./asyncActions";
-import { CollectionItem } from "@/modules/CollectionModule";
+import { CollectionsList } from "@/components/CollectionsList";
 
 const mapStateToProps = (rootState: RootState) => ({
   collections: rootState.collectionsList.collections,
@@ -31,22 +31,12 @@ const CollectionsListComponent = ({
       getAllCollections();
     }
   }, [isActual]);
-  return (
-    <div>
-      <Typography.Title level={3}>Список коллекций</Typography.Title>
-      {(!collections || collections.length === 0) && (
-        <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
-      )}
-      {collections?.map((collection) => (
-        <CollectionItem
-          key={collection.title}
-          collectionInfo={collection}
-          // title={collection.title}
-          // description={collection.description}
-        />
-      ))}
-    </div>
-  );
+
+  if (!collections || collections.length === 0) {
+    return <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />;
+  }
+
+  return <CollectionsList collections={collections} />;
 };
 
 export const CollectionsListModule = connect(
